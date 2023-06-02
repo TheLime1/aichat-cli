842d5371cbeda1a1db27412679c5be761d017d25
Aymen Hmani <47940043+TheLime1@users.noreply.github.com>
2023-06-02 21:51:19 +0100

    test1

--- bad_token_remover.py
+++ bad_token_remover.py
@@ -1,43 +1,56 @@
+# this file is compatible with github actions
+
 import poe
 import os
 
 
-def remove_bad_tokens(dir):
-    premium_tokens_file = os.path.join(dir, "tokens", "premium_tokens.txt")
-    unchecked_tokens = []
+def remove_bad_tokens():
+    # Get the current working directory, which is the repository root in GitHub Actions
+    repo_root = os.getcwd()
+
+    premium_tokens_file = os.path.join(
+        repo_root, "tokens", "premium_tokens.txt")
     valid_tokens = []
+    unchecked_tokens = []
     bad_tokens = []
     consecutive_valid_tokens = 0
 
     try:
         with open(premium_tokens_file, 'r') as f:
             premium_tokens = f.read().splitlines()
-            unchecked_tokens = premium_tokens.copy()
 
-        for token in premium_tokens:
+        # Move all tokens to the unchecked_tokens list initially
+        unchecked_tokens.extend(premium_tokens)
+
+        for token in unchecked_tokens:
             try:
                 client = poe.Client(token)
                 valid_tokens.append(token)
                 consecutive_valid_tokens += 1
-                unchecked_tokens.remove(token)
-                print(f"Valid token: {token}")
 
                 if consecutive_valid_tokens == 3:
-                    break
+                    break  # Stop the program when there are three consecutive valid tokens
 
             except RuntimeError:
                 bad_tokens.append(token)
-                unchecked_tokens.remove(token)
                 print(f"Removing bad token: {token}")
-                consecutive_valid_tokens == 0
+                consecutive_valid_tokens = 0
 
         with open(premium_tokens_file, 'w') as f:
-            f.write('\n'.join(valid_tokens + unchecked_tokens))
+            f.write('\n'.join(valid_tokens))
 
     except FileNotFoundError:
         print("premium_tokens.txt file not found.")
 
+    print("Valid tokens:")
+    print(valid_tokens)
+
+    print("Unchecked tokens:")
+    print(unchecked_tokens)
+
+    print("Bad tokens:")
+    print(bad_tokens)
+
 
 # Usage example
-dir = os.path.dirname(os.path.abspath(__file__))
-remove_bad_tokens(dir)
+remove_bad_tokens()
