b52e3bdd4225284183cde3c53c91720c731a1214
Aymen Hmani <47940043+TheLime1@users.noreply.github.com>
2023-06-10 20:43:21 +0100

    update bing

--- gptcli/bingfunc.py
+++ gptcli/bingfunc.py
@@ -1,18 +1,22 @@
-from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
+import json
+from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle, NotAllowedToAccess
+import asyncio
 
 
 async def bingbot(prompt: str, style: ConversationStyle):
     bot = await Chatbot.create()  # Passing cookies is optional
-    response = await bot.ask(prompt=prompt, conversation_style=style)
-    print(response['item']['messages'][1]['text'])
+
+    try:
+        response = await bot.ask(prompt=prompt, conversation_style=style, simplify_response=True)
+        print(response["text"])
+        return response["text"]
+    except NotAllowedToAccess:
+        # using token here
+        print("using token")
+
     await bot.close()
-    return response['item']['messages'][1]['text']
-
-# ConversationStyle.creative
-# ConversationStyle.balanced
-# ConversationStyle.precise
 
 
-# prompt = "count to 3"
-# style = ConversationStyle.creative
-# asyncio.run(bingbot(prompt, style))
+prompt = "count to 3"
+style = ConversationStyle.creative
+result = asyncio.run(bingbot(prompt, style))
