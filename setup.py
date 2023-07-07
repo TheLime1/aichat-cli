ccc9fd29b8d9b80353eb487523e12738a6495f50
TheLime03 <everpadd4@gmail.com>
2023-07-07 18:19:48 +0100

    update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="aichat-cli",
-    version="0.4.9",
+    version="0.5.0",
     author="TheLime1",
     license="GPLv3",
     description="A CLI app that allows you to have interactive conversations with different AI bots",
